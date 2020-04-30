f1f395d93263ef2cca5e198cc11f8a94f62c9763
Alin Enache <alinenache@Alins-MacBook.local>
2020-04-30 14:12:27 +0100

    master

---
+++ main.js
@@ -0,0 +1,11 @@
+function changePic() {
+  document.getElementById("profilePic").src = "images/profilepic.jpg";
+}
+  
+function normalPic() {
+  document.getElementById("profilePic").src = "images/profilepic.jpg";
+}
+
+function cvPage(){
+  window.open("https://alincristianenache.github.io/Assignment2/index.html");
+}
